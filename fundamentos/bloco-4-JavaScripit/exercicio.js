4ceb4a30a23cf6fe8251dfcce8b393f192886987
Magalhães,JVA <jv.andrademagalhaes@gmail.com>
2022-06-27 14:31:56 -0300

    Aula ao vivo

---
+++ fundamentos/bloco-4-JavaScripit/exercicio.js
@@ -0,0 +1,10 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+
+console.log(menuServices);
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOfPortfolio('Portfólio');
+
+console.log(indexOfPortfolio);
